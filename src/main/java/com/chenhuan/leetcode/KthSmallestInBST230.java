f42e7f8914f026676ed88b90e6c85a067c2aeee9
chenhuan09 <chenhuan09@meituan.com>
2018-12-30 18:20:11 +0800

    Iterative BFS

--- src/main/java/com/chenhuan/leetcode/KthSmallestInBST230.java
+++ src/main/java/com/chenhuan/leetcode/KthSmallestInBST230.java
@@ -1,8 +1,44 @@
 package com.chenhuan.leetcode;
 
+import java.util.LinkedList;
+
 public class KthSmallestInBST230 {
     int count;
     int val;
+    /**
+     * Iterative BFS
+     * @param root
+     * @param k
+     */
+
+    public int kthSmallestIterativeBFS(TreeNode root, int k) {
+        LinkedList<TreeNode> stack = new LinkedList<TreeNode>();
+
+        pushNodeAndLeft(stack, root);
+        while(stack.peekLast() != null) {
+            TreeNode node = stack.pollLast();
+            if (++count == k) {
+                return node.val;
+            }
+            if (node.right != null) {
+                pushNodeAndLeft(stack, node.right);
+            }
+        }
+        return -1;
+    }
+
+    void pushNodeAndLeft (LinkedList<TreeNode> stack , TreeNode node) {
+        while (node != null) {
+            stack.offerLast(node);
+            node = node.left;
+        }
+    }
+
+    /**
+     * Recursive BFS
+     */
+
+
     public int kthSmallestRecursiveBFS(TreeNode root, int k) {
         inorderRecursive(root, k);
         return val;
@@ -16,6 +52,12 @@ public class KthSmallestInBST230 {
         }
         inorderRecursive(node.right, k);
     }
+
+    /**
+     * DFS
+     * @param root
+     * @param k
+     */
 
 
     public int kthSmallestDFS(TreeNode root, int k) {
